f5233dc2609b9030c0613a34b16f195d833cef58
nicolascasmuz <nicolascasmuz@hotmail.com>
2022-10-11 12:51:00 -0300

    commit-002

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
-import { init as initHeader } from "./components/header-comp/header-comp";
-import { init as initH1 } from "./components/h1-comp/h1-comp";
-import { init as initForm } from "./components/form-comp/form-comp";
-import { init as initCard } from "./components/card-comp/card-comp";
+import { initHomepage } from "./pages/home/index";
+import "./components/header";
+import "./components/footer";
+import "./components/form";
+import "./components/todo-item/";
+import { state } from "./state";
 
-initHeader();
-initH1();
-initForm();
-initCard();
+(function () {
+  /* state.init(); */
+  initHomepage(document.querySelector(".wrapper"));
+})();
--- src/state.ts
+++ src/state.ts
@@ -1,18 +1,33 @@
 export const state = {
   data: {
-    task: "",
+    tasks: [],
   },
   listeners: [],
+  /* init() {
+    const localData: any = localStorage.getItem("saved-state");
+    this.setState(JSON.parse(localData));
+    localStorage.clear();
+  }, */
   getState() {
     return this.data;
   },
   setState(newState) {
     this.data = newState;
     for (const cb of this.listeners) {
-      cb();
+      cb(newState);
     }
+    /* localStorage.setItem("saved-state", JSON.stringify(newState)); */
   },
   subscribe(callback: (any) => any) {
     this.listeners.push(callback);
+  },
+  getEnabledTasks() {
+    const currentState = this.getState();
+    return currentState.tasks.filter((t) => !t.deleted);
+  },
+  addTask(title) {
+    const currentState = this.getState();
+    currentState.tasks.push({ title, completed: false });
+    this.setState(currentState);
   },
 };
